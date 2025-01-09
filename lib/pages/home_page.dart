c50d0703bc7078a31596bcb1f000fadccd4d62e9
sarvarAbdumajidov <abdumajidovsarvarbek07@gmail.com>
2025-01-09 21:55:59 +0500

    Flutter Secure Storage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:learn_shared_preferences/model/user_model.dart';
 import 'package:learn_shared_preferences/service/log_service.dart';
 import 'package:learn_shared_preferences/service/prefs_service.dart';
+import 'package:learn_shared_preferences/service/secure_service.dart';
+import 'package:logger/logger.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,9 +16,11 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    var user = User('7', 'sarvar@gmail.com', '1904');
-    PrefService.storeUser(user);
-    PrefService.loadUser().then((v) => {LogService.i(v! .toJson().toString())});
+    // var user = User('7', 'sarvar@gmail.com', '1904');
+    // PrefService.storeUser(user);
+    // PrefService.loadUser().then((v) => {LogService.i(v! .toJson().toString())});
+    SecureService.storeApiKey('AIzaSyDaGmWKa4JsXZ-HjGw7ISLn_3namBGewQe');
+    SecureService.loadApiKey().then((value) => LogService.w(value!));
   }
 
   @override
